22f2d9dafef58973743a7c8413b0b35550441e79
JackYoung33 <39749414+JackYoung33@users.noreply.github.com>
2019-04-19 13:54:55 -0500

    Update Player_Class.py

--- Player_Class.py
+++ Player_Class.py
@@ -1,9 +1,15 @@
 #create player class
 
 class Player:
+
     def __init__(self, name, role, HP, AD, Def):
         self.name = name
         self.role = role
         self.HP = HP
         self.AD = AD
         self.Def = Def
+
+
+
+
+
