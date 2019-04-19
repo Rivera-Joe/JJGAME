ad958881e43a57f099649ab83aad7fd7d8b053ad
JackYoung33 <39749414+JackYoung33@users.noreply.github.com>
2019-04-19 14:34:16 -0500

    Update Level_1.py

--- Level_1.py
+++ Level_1.py
@@ -1,42 +1,55 @@
-#function for level 1
-#started on 04/17/2019
-#Joe
+# function for level 1
+# started on 04/17/2019
+# Joe
 
-#Import player class
+# Import player class
 import Player_Class
 
+
+class Player:
+
+    def __init__(self, name, role, HP, AD, Def):
+        self.name = name
+        self.role = role
+        self.HP = HP
+        self.AD = AD
+        self.Def = Def
+
+
 def levelOne():
-#stats for Knight
+    role = ' '
+    # stats for Knight
     Knight = "Knight"
     kHP = 100
     kAD = 20
     kDef = 30
 
-#stats for Assassin
+    # stats for Assassin
     Assassin = "Assasssin"
     aHP = 50
     aAD = 50
     aDef = 10
 
-#gets name for player
+    # gets name for player
     name = input("So you're back again, eh? What was your name again?\n")
-    
-    
+    print('hello ' + name)
 
-#make sure player chooses correct role
-    while(role !=Knight or role !=Assassin):
+    # make sure player chooses correct role
+    while (True):
         role = input("Choose your role (Assassin or Knight )\n")
-        r = role.lower()
+        role = role.lower()
+        if (role == 'assassin') or (role == "knight"):
+            break
 
-#sets players stats based on the role selected
-    if(r == "assassin"):
-       p1 = Player(name, Assassin, aHP, aAD, aDef)
+    # sets players stats based on the role selected
+    if (role == "assassin"):
+        p1 = Player(name, Assassin, aHP, aAD, aDef)
+        print('you are an assassin')
     else:
-       p1 = Player(name, Knight, kHP, kAD, kDef)
+        p1 = Player(name, Knight, kHP, kAD, kDef)
+        print('you are a knight')
 
 
-
-#print(Player_Class.role)
+    print(p1.role)
 
 levelOne()
-
