206971bf26ce285657c7068560b0e86a92a16aea
Rivera-Joe <36744600+Rivera-Joe@users.noreply.github.com>
2019-04-21 19:01:09 -0500

    removed extra class def
    removed extra class definition from Level 1

--- Level_1.py
+++ Level_1.py
@@ -10,15 +10,6 @@ from Player_Class import Player
 
 
 
-class Player:
-
-    def __init__(self, name, role, HP, AD, Def):
-        self.name = name
-        self.role = role
-        self.HP = HP
-        self.AD = AD
-        self.Def = Def
-
 
 def levelOne():
 
